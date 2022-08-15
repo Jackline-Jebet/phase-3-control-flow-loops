7eda0683cb99911fcdd3852785f34560b3bcf27f
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-15 16:54:09 +0300

    commit tree 6bbddd5d7e9bc7ad5c2cbdd82f7c98f8cc31c412

--- looping.rb
+++ looping.rb
@@ -1,6 +1,15 @@
+require 'pry'
+
 def happy_new_year
-  # your code here
+  countdown = 10
+   while countdown > 0
+    puts countdown
+    countdown -= 1
+   end
+   puts "Happy New Year!"
 end
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +24,21 @@ def fizzbuzz(num)
   end
 end
 
+
+
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |i|
+    puts fizzbuzz(i)
+  end
 end
 
+
+
+
 def reverse_string(str)
-  # your code here
+  reversedStr = "";
+  str.length.times do |i|
+    reversedStr = str[i] + reversedStr;
+  end
+  reversedStr
 end
